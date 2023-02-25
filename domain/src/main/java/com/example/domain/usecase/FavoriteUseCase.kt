4c410a2097e879c13a3b4f49430495a3dd464e6a
Omar Mohamed <98606902+OmarLkhalil@users.noreply.github.com>
2023-02-25 20:11:58 +0200

    Delete FavoriteUseCase.kt

--- domain/src/main/java/com/example/domain/usecase/FavoriteUseCase.kt
+++
@@ -1,9 +0,0 @@
-package com.example.domain.usecase
-
-import com.example.domain.entity.FavoriteMeal
-import com.example.domain.dao.FavoriteMealDao
-import kotlinx.coroutines.flow.Flow
-
-class FavoriteUseCase(private val favRepo: FavoriteMealDao) {
-    operator fun invoke(): Flow<List<FavoriteMeal>> = favRepo.getAll()
-}
